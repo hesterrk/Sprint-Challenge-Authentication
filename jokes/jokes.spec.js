1c41cd776c194ffc2090ca1eebf6226a755d4579
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-27 09:42:42 +0000

    added another test for jokes route with token provided, works, tested

--- jokes/jokes.spec.js
+++ jokes/jokes.spec.js
@@ -4,10 +4,21 @@ var jwt = require('jsonwebtoken')
 
 
 
-test("testing jokes route, no auth header", async () => {
+test("testing jokes route, no token provided", async () => {
     const res = await supertest(server).get("/api/jokes");
     expect(res.statusCode).toBe(401);
     expect(res.type).toBe("application/json");
     expect(res.unauthorized).toBe(true);
     expect(res.body.message).toMatch(/you shall not pass/i);
+  });
+
+  test("testing jokes route, token provided", async () => {
+    var token = jwt.sign({
+      id: 1
+    }, process.env.JWT_SECRET="hello")
+    const res = await supertest(server).get("/api/jokes")
+    .set('Authorization', token)
+    expect(res.statusCode).toBe(200);
+    expect(res.type).toBe("application/json");
+    expect(res.unauthorized).toBe(false);
   });
