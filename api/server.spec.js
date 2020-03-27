a1dbeac1143589d9ff16116cf88e7cd58af69969
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-27 08:40:29 +0000

    another test for server file

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,23 @@
+const supertest = require("supertest");
+const server = require("./server");
+const db = require("../database/dbConfig");
+
+beforeEach(async () => {
+  await db.seed.run();
+});
+
+
+test("home route", async () => {
+  const res = await supertest(server).get("/");
+  expect(res.statusCode).toBe(200);
+  expect(res.type).toBe("application/json");
+  expect(res.body.message).toBe("all working");
+  expect(res.body.message).toHaveLength(11);
+  expect(res.body.message).toMatch(/all/i);
+});
+
+
+
 test("server enviroment", () => {
     expect(process.env.DB_ENV).toBe("testing")
   })
