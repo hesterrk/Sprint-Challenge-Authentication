3e2c76f7e9a784e24c5929208ce1331887551300
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-27 09:28:37 +0000

    more tests for my auth endpoints with and without auth, all working

--- users/users.spec.js
+++ users/users.spec.js
@@ -29,3 +29,33 @@ test("getting list of all users with authorisation", async () => {
       expect(res.statusCode).toBe(200)
       expect(res.body.length).toBe(3)
     });
+
+
+    test("register route, no token", async () => {
+        const res = await supertest(server)
+          .post("/api/auth/register")
+          .send({ username: "Hanna", password: "1234" });
+        expect(res.type).toBe("application/json");
+      });
+
+
+
+      test("register route, token generated", async () => {
+        var token = jwt.sign({
+          id: 1
+        }, process.env.JWT_SECRET="hello")
+          const res = await supertest(server).post("/api/auth/register")
+          .send({ username: "Hanna", password: "1234" })
+          .set('Authorization', token)
+          expect(res.statusCode).toBe(201)
+        });
+
+        test("login route, no token", async () => {
+            const res = await supertest(server)
+              .post("/api/auth/login")
+              .send({ username: "Hanna", password: "1234" });
+            expect(res.type).toBe("application/json");
+            expect(res.statusCode).toBe(401);
+            expect(res.body.message).toMatch(/invalid credentials/i);
+            
+          });
